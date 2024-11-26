e1c31d00f5239769a0f0f9f3f061507ed560768b
Abdurrahman <aboodmm@gmail.com>
2024-11-26 15:38:08 -0800

    fix structure

--- src/models/receipt.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type receipt struct {
-	Retailer     string              `json:"retailer"`
-	PurchaseDate string              `json:"purchaseDate"`
-	PurchaseTime string              `json:"purchaseTime"`
-	Total        string              `json:"total"`
-	Items        []map[string]string `json:"items"`
-}
